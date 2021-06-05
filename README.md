72f0976cf524305cf48e93f4dc51bdbd03bd802b
Tudor Hulban <prindex2010@gmail.com>
2021-06-05 12:29:07 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Homework-CompanyDirectory
+# Homework - Company Directory
+Hierarchical organisation has below hierarchy of employees:<br/>
+a. CEO<br/>
+b. Manager. Has at least one employee reporting to him / her.<br/>
+c. Employee. Reports to Manager.
+
+
+## To Do
+Implement a corporate directory to find the closest common Manager (aka farthest from the CEO) between two employees. <br/>
+Assumption: All employees eventually report up to the CEO.
+
+### Guidelines
+Replace ambiguity with assumptions.<br/>
+The directory should be an in-memory structure.<br/>
+A Manager should link to Employees and not the other way around.<br/>
+Flexible on wow the program takes its input and produces its output.<br/>
+
+## Solution
+Remaining<br/>
+a. load employees from CSV for example on init
+b. get employee nodes based on passed params
+
---
+++ t_lineage_test.go
@@ -0,0 +1,101 @@
+package company
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLineage(t *testing.T) {
+	company := NewCompany()
+
+	_, errNil := company.FirstManager(nil, nil)
+	require.Equal(t, errNil, errNilEmployee)
+
+	ceo := &Employee{
+		FullName: "Monica",
+	}
+
+	_, errZero := company.FirstManager(nil, nil)
+	require.NotNil(t, errZero)
+
+	require.Nil(t, company.Add(ceo), "adding root employee")
+
+	assert.Equal(t, 1, len(company.team))
+	require.Nil(t, company.Hierarchy(ceo))
+
+	m1 := &Employee{
+		FullName: "Marcus",
+	}
+
+	require.Equal(t, company.Add(m1), errNilManager)
+
+	m1.Manager = ceo
+
+	require.NotNil(t, company.Hierarchy(m1)[0])
+	assert.Equal(t, m1, company.Hierarchy(m1)[0])
+
+	w1 := &Employee{
+		Manager:  m1,
+		FullName: "John Doe",
+	}
+	assert.Nil(t,company.Add(w1))
+	
+	w2 := &Employee{
+		Manager:  m1,
+		FullName: "Bassem Al Raed",
+	}
+	assert.Nil(t, company.Add(w2))	
+
+	m2 := &Employee{
+		Manager:  ceo,
+		FullName: "Andre",
+	}
+		assert.Nil(t, company.Add(m2))	
+
+	w3 := &Employee{
+		Manager:  m2,
+		FullName: "Maurice Ravel",
+	}
+
+	assert.Nil(t, company.Add(w3))	
+
+	m3 := &Employee{
+		Manager:  m1,
+		FullName: "Constantin",
+	}
+
+	assert.Nil(t, company.Add(m3))	
+
+	w4 := &Employee{
+		Manager:  m2,
+		FullName: "Elena",
+	}
+	
+	assert.Nil(t, company.Add(w4))	
+
+	// testing lineage
+	cases := []struct {
+		description string
+		expectError bool
+		a, b        *Employee
+		expected    *Employee
+	}{
+		{"Lineage CEO", false, ceo, ceo, nil},
+		{"No lineage but CEO", true, w1, w4, ceo},
+		{"Same Team Workers", false, w1, w2, m1},
+		{"Board Managers", true, m1, m2, ceo},
+	}
+
+	for _, tc := range cases {
+		firstManager, err := company.FirstManager(tc.a, tc.b)
+
+		assert.Equal(t, tc.expectError, err != nil)
+		assert.Equal(t, tc.expected, firstManager)
+	}
+}
+
+func TestList(t *testing.T) {
+	// TODO: add testing for List
+}
